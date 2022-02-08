615aa3f6f28db31430416c8dfa4263680547bf73
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-02-08 12:42:34 +0000

    on it

--- spec/arrays.spec.js
+++ spec/arrays.spec.js
@@ -1,57 +1,56 @@
 const f = require('../src/arrays')
 
 describe("Arrays", () => {
-  
-  it("findFirstNumberGreaterThan10", () => {
-    expect(f.findFirstNumberGreaterThan10()).toEqual(76)
-  })
 
-  it("findFirstNumberLessThan10", () => {
-    expect(f.findFirstNumberLessThan10()).toEqual(5)
-  })
-  
-  it("findFirstItemWithQuantityOne", () => {
-    expect(f.findFirstItemWithQuantityOne()).toEqual({ item: 'banana', quantity: 1 })
-  })
+    it("findFirstNumberGreaterThan10", () => {
+        expect(f.findFirstNumberGreaterThan10()).toEqual(76)
+    })
 
-  it("findIndexTom", () => {
-    expect(f.findIndexTom()).toEqual(2)
-  })
+    fit("findFirstNumberLessThan10", () => {
+        expect(f.findFirstNumberLessThan10()).toEqual(5)
+    })
 
-  it("findIndexLemon", () => {
-    expect(f.findIndexLemon()).toEqual(3)
-  })
+    fit("findFirstItemWithQuantityOne", () => {
+        expect(f.findFirstItemWithQuantityOne()).toEqual({ item: 'banana', quantity: 1 })
+    })
 
-  it("allOver100", () => {
-    expect(f.allOver100()).toEqual([101, 2001, 198])
-  })
+    fit("findIndexTom", () => {
+        expect(f.findIndexTom()).toEqual(2)
+    })
 
-  it("startsWithM", () => {
-    expect(f.startsWithM()).toEqual(['Matt','Marie','Mike'])
-  })
+    fit("findIndexLemon", () => {
+        expect(f.findIndexLemon()).toEqual(3)
+    })
 
-  it("horrorFilms", () => {
-    expect(f.horrorFilms()).toEqual([
-      { 
-        name: 'The Manor',
-        genre: 'Horror'
-      },
-      { 
-        name: 'In Ehe Earth',
-        genre: 'Horror'
-      },
-    ])
-  })
+    fit("allOver100", () => {
+        expect(f.allOver100()).toEqual([101, 2001, 198])
+    })
 
-  it("doubleEachNumber", () => {
-    expect(f.doubleEachNumber()).toEqual([2,20,200,2000])
-  })
+    fit("startsWithM", () => {
+        expect(f.startsWithM()).toEqual(['Matt', 'Marie', 'Mike'])
+    })
 
-  it("sumNumbers", () => {
-    expect(f.sumNumbers()).toEqual(1111)
-  })
+    it("horrorFilms", () => {
+        expect(f.horrorFilms()).toEqual([{
+                name: 'The Manor',
+                genre: 'Horror'
+            },
+            {
+                name: 'In Ehe Earth',
+                genre: 'Horror'
+            },
+        ])
+    })
 
-  fit("findLargest", () => {
-    expect(f.findLargest()).toEqual(9121)
-  })
+    it("doubleEachNumber", () => {
+        expect(f.doubleEachNumber()).toEqual([2, 20, 200, 2000])
+    })
+
+    it("sumNumbers", () => {
+        expect(f.sumNumbers()).toEqual(1111)
+    })
+
+    fit("findLargest", () => {
+        expect(f.findLargest()).toEqual(9121)
+    })
 })
--- src/arrays.js
+++ src/arrays.js
@@ -1,109 +1,150 @@
 //Example:
 function findFirstNumberGreaterThan10() {
-  const nums = [5, 2, 1, 76, 12]
-  
-  return nums.find(function(num) {
-    return num > 10
-  })
+    const nums = [5, 2, 1, 76, 12]
+
+    return nums.find(function(num) {
+        return num > 10
+    })
 }
 
 function findFirstNumberLessThan10() {
-  const nums = [12, 34, 5, 12, 99]
-  
-  //TODO: Use items.find to return the first item in the array with a quantity of 1
+    const nums = [12, 34, 5, 12, 99]
+
+    //TODO: Use items.find to return the first item in the array with a quantity of 1
+    return nums.find(function(num) {
+        return num < 10
+    })
 }
 
 function findFirstItemWithQuantityOne() {
-  const items = [
-    { item: 'apple', quantity: 2 },
-    { item: 'orange', quantity: 10 },
-    { item: 'banana', quantity: 1 },
-    { item: 'lemon', quantity: 2 }
-  ]
+    const items = [
+        { item: 'apple', quantity: 2 },
+        { item: 'orange', quantity: 10 },
+        { item: 'banana', quantity: 1 },
+        { item: 'lemon', quantity: 2 }
+    ]
 
-  //TODO: Use items.find to return the first item in the array with a quantity of 1
+    //TODO: Use items.find to return the first item in the array with a quantity of 1
+    return items.find(function(items) {
+        return items.quantity === 1
+
+
+    })
+
 }
 
 function findIndexTom() {
-  const names = ['bob', 'jane', 'tom', 'fred']
-  
-  //TODO: Use names.findIndex to return the index of 'tom' in the array
+    const names = ['bob', 'jane', 'tom', 'fred']
+    return names.findIndex(function(name) {
+        if (name === 'tom') {
+            return true
+        } else {
+            return false
+        }
+    })
+
+
+
+
+    //TODO: Use names.findIndex to return the index of 'tom' in the array
+
 }
 
 function findIndexLemon() {
-  const items = [
-    { item: 'apple', quantity: 2 },
-    { item: 'orange', quantity: 10 },
-    { item: 'banana', quantity: 1 },
-    { item: 'lemon', quantity: 2 }
-  ]
-  
-  //TODO: Use items.findIndex to return the index of the item 'lemon' in the array
+    const items = [
+        { item: 'apple', quantity: 2 },
+        { item: 'orange', quantity: 10 },
+        { item: 'banana', quantity: 1 },
+        { item: 'lemon', quantity: 2 }
+    ]
+
+    //TODO: Use items.findIndex to return the index of the item 'lemon' in the array
+    return items.findIndex(function(items) {
+        if (items.item === 'lemon') {
+            return true
+        } else {
+            return false
+        }
+    })
+
 }
 
 function allOver100() {
-  const nums = [13, 16, 99, 101, 2001, 198, 10]
-  
-  //TODO: Use nums.filter to return an array containing every item nums that is over 100
+    const nums = [13, 16, 99, 101, 2001, 198, 10]
+
+    //TODO: Use nums.filter to return an array containing every item nums that is over 100
+    return nums.filter(function(nums) {
+        if (nums > 100) {
+            return true
+        } else {
+            return false
+        }
+    })
 }
 
 function startsWithM() {
-  const names = ['Matt', 'Bob', 'Marie', 'Fred', 'Mike', 'Charles']
-  
-  //TODO: Use names.filter to return an array containing every name starting with an 'M'
+    const names = ['Matt', 'Bob', 'Marie', 'Fred', 'Mike', 'Charles']
+
+    //TODO: Use names.filter to return an array containing every name starting with an 'M'
+    return names.filter(function(name) {
+        if (name.includes('M')) {
+            return true
+        } else {
+            return false
+        }
+    })
 }
 
 function horrorFilms() {
-  const films = [
-    { 
-      name: 'The Manor',
-      genre: 'Horror'
-    },
-    { 
-      name: 'Dune',
-      genre: 'SciFi'
-    },
-    { 
-      name: 'Dont Look Up',
-      genre: 'Comedy'
-    },
-    { 
-      name: 'In Ehe Earth',
-      genre: 'Horror'
-    },
-  ]
-  
-  //TODO: Use films.filter to return an array containing all horror films
+    const films = [{
+            name: 'The Manor',
+            genre: 'Horror'
+        },
+        {
+            name: 'Dune',
+            genre: 'SciFi'
+        },
+        {
+            name: 'Dont Look Up',
+            genre: 'Comedy'
+        },
+        {
+            name: 'In Ehe Earth',
+            genre: 'Horror'
+        },
+    ]
+
+    //TODO: Use films.filter to return an array containing all horror films
 }
 
 function doubleEachNumber() {
-  const nums = [1, 10, 100, 1000]
-  
-  //TODO: Use nums.map to return an array containing each number doubled
+    const nums = [1, 10, 100, 1000]
+
+    //TODO: Use nums.map to return an array containing each number doubled
 }
 
 function sumNumbers() {
-  const nums = [1, 10, 100, 1000]
-  
-  //TODO: Use nums.reduce to return the sum of all items in the array
+    const nums = [1, 10, 100, 1000]
+
+    //TODO: Use nums.reduce to return the sum of all items in the array
 }
 
 function findLargest() {
-  const nums = [341, 120, 9121, 862]
+    const nums = [341, 120, 9121, 862]
 
-  //TODO: Use nums.reduce to return the largest number in the array
+    //TODO: Use nums.reduce to return the largest number in the array
 }
 
 module.exports = {
-  findFirstNumberGreaterThan10 : findFirstNumberGreaterThan10,
-  findFirstNumberLessThan10 : findFirstNumberLessThan10,
-  findFirstItemWithQuantityOne : findFirstItemWithQuantityOne,
-  findIndexTom : findIndexTom,
-  findIndexLemon : findIndexLemon,
-  allOver100 : allOver100,
-  startsWithM : startsWithM,
-  horrorFilms : horrorFilms,
-  doubleEachNumber: doubleEachNumber,
-  sumNumbers: sumNumbers,
-  findLargest: findLargest
+    findFirstNumberGreaterThan10: findFirstNumberGreaterThan10,
+    findFirstNumberLessThan10: findFirstNumberLessThan10,
+    findFirstItemWithQuantityOne: findFirstItemWithQuantityOne,
+    findIndexTom: findIndexTom,
+    findIndexLemon: findIndexLemon,
+    allOver100: allOver100,
+    startsWithM: startsWithM,
+    horrorFilms: horrorFilms,
+    doubleEachNumber: doubleEachNumber,
+    sumNumbers: sumNumbers,
+    findLargest: findLargest
 }
